d2ab365ad6d7cb6544c592f56cf996c8c164ab2b
Paul Walsh <paulywalsh@gmail.com>
2013-10-30 21:39:43 +0200

    Silence contrib,fabric for now

--- dock/contrib/fabric/config.py
+++ dock/contrib/fabric/config.py
@@ -1,20 +1,20 @@
-import datetime
-from fabric.api import env
-
-
-env.user = ''
-
-env.roledefs = {
-    'web': ['']
-}
-
-CONFIG = {
-    'user': env.user,
-    'dataset_root': '',
-    'dataset_repo': '',
-    'dataset_branch': '',
-    'db_name': '',
-    'db_user': '',
-    'db_dump_file': '',
-    'timestamp': datetime.datetime.now(),
-}
+#import datetime
+#from fabric.api import env
+#
+#
+#env.user = ''
+#
+#env.roledefs = {
+#    'web': ['']
+#}
+#
+#CONFIG = {
+#    'user': env.user,
+#    'dataset_root': '',
+#    'dataset_repo': '',
+#    'dataset_branch': '',
+#    'db_name': '',
+#    'db_user': '',
+#    'db_dump_file': '',
+#    'timestamp': datetime.datetime.now(),
+#}
